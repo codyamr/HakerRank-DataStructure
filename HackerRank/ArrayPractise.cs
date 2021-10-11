c629466107383e5b08db9c54d8fbc66a54e3a719
amr.awnyameen <amr.awnyameen@gmail.com>
2021-10-11 17:50:21 +0200

    Array-2dArray-Problem

--- HackerRank/ArrayPractise.cs
+++ HackerRank/ArrayPractise.cs
@@ -9,6 +9,7 @@ namespace HackerRank
    public class ArrayPractise
     {
 
+        //Ds
         public static List<int> reverseArray(List<int> a)
         {
             //{ 1, 5, 8, 9 };
@@ -18,6 +19,43 @@ namespace HackerRank
                 newListItem.Add(a.ToArray()[i]);
             }
             return newListItem;
+        }
+
+        //2DArray
+        public static int hourglassSum(int[][] arr)
+        {
+
+
+            int maxSum = int.MinValue;
+            int rows = arr.Length - 2;
+            for (int j = 0; j < rows; j++)
+            {
+                //0 1 2 3 4 5
+                //0 1 2 3 4 5
+                //0 1 2 3 4 5
+                //0 1 2 3 4 5
+                //0 1 2 3 4 5
+                //0 1 2 3 4 5
+                int columns = arr[j].Length - 2;
+
+                for (int k = 0; k < columns; k++)
+                {
+                    var xxxxxx1 = k; var xxxxxx2 = j;
+
+
+
+
+                    int sum = arr[j][k] + arr[j][k + 1] + arr[j][k + 2]
+                                        + arr[j + 1][k + 1]
+                        + arr[j + 2][k] + arr[j + 2][k + 1] + arr[j + 2][k + 2];
+
+                    if (maxSum < sum)
+                    {
+                        maxSum = sum;
+                    }
+                }
+            }
+            return maxSum;
         }
     }
 }
